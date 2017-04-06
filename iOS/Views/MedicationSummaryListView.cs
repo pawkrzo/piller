d2c96248f401849bbe84fb713dbf727d36161364
Mazur.Michal YMH <ymh@kmd.dk>
2017-04-06 14:56:41 +0200

    iOS: added medication summary list

---
+++ iOS/Views/MedicationSummaryListView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MvvmCross.iOS.Views;
+using Piller.ViewModels;
+using MvvmCross.Binding.BindingContext;
+
+namespace Piller.iOS.Views
+{
+    public class MedicationSummaryListView : MvxTableViewController<MedicationSummaryListViewModel>
+    {
+        public MedicationSummaryListView ()
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            this.TableView.Source = new MedicationSummaryListViewSource (this.TableView);
+
+            var addButton = new UIKit.UIBarButtonItem (UIKit.UIBarButtonSystemItem.Add);
+            this.NavigationItem.RightBarButtonItem = addButton;
+
+            var bindingSet = this.CreateBindingSet<MedicationSummaryListView, MedicationSummaryListViewModel> ();
+            bindingSet.Bind (addButton).To (vm => vm.AddNew);
+            bindingSet.Apply ();
+        }
+    }
+}
---
+++ iOS/Views/MedicationSummaryListViewSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Binding.iOS.Views;
+using UIKit;
+
+namespace Piller.iOS.Views
+{
+    public class MedicationSummaryListViewSource : MvxTableViewSource
+    {
+        public MedicationSummaryListViewSource (UITableView tableView) : base(tableView)
+        {
+        }
+
+        protected override UITableViewCell GetOrCreateCellFor (UITableView tableView, NSIndexPath indexPath, object item)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
