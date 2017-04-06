eb1a6b992f7039d8e9296b9bd898e453ec35f855
Mazur.Michal YMH <ymh@kmd.dk>
2017-04-06 16:08:54 +0200

    Added storage service to core

---
+++ Services/IPermanentStorageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Piller.Services
+{
+    public interface IPermanentStorageService
+    {
+        Task<T> GetAsync<T> (int id);
+        Task SaveAsync<T> (T entity);
+    }
+}
--- Services/PermanentStorageService.cs
+++ Services/PermanentStorageService.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 namespace Piller.Services
 {
-    public class PermanentStorageService
+    public class PermanentStorageService : IPermanentStorageService
     {
-        
+        public Task<T> GetAsync<T> (int id)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task SaveAsync<T> (T entity)
+        {
+            throw new NotImplementedException ();
+        }
     }
 }
